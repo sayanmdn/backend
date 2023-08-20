84c6f26d86438812798b677d2d9ed8a1a1b7ae3f
Sayantan Mishra <sayanmdn@gmail.com>
2023-08-20 18:35:14 +0530

    backend update for mumbai deployment

--- src/index-local.ts
+++ src/index-local.ts
@@ -1,17 +1,17 @@
 import express from "express";
-import serverless from "serverless-http";
-import { connect } from "mongoose";
-import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import authRouter from "./routes/auth";
 import postRoute from "./routes/private";
+import { connect, Connection, connection } from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+const port = 8080;
 
 app.use("/user", authRouter);
 app.use("/post", postRoute);
@@ -25,10 +25,19 @@ const mongo_password: string | undefined = process.env.MONGO_PASSWORD;
 
 const uri: string = `mongodb+srv://${username}:${mongo_password}@backend-serverless.3e0kv62.mongodb.net/portfolio-db?retryWrites=true&w=majority`;
 
+console.log("uri ", uri);
+
 try {
   connect(uri);
+
+  const dbConnection: Connection = connection;
+  dbConnection.once("open", function () {
+    console.log("MongoDB database connection established successfully");
+  });
 } catch (error) {
   console.log("Error while connection Mongodb Atlas ", error);
 }
 
-module.exports.handler = serverless(app);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
