8d401fbf0430a9963ebfc43fd93a9677027eefba
Sayantan Mishra <sayanmdn@gmail.com>
2023-12-04 16:53:10 +0530

    password revealing fixed

--- src/routes/teachers.ts
+++ src/routes/teachers.ts
@@ -14,6 +14,26 @@ router.post("/find", async (req: Request, res: Response) => {
     const returnedData = await userModel.find({
       $or: [{ name: { $regex: new RegExp(data, "i") } }, { subjects: { $regex: new RegExp(data, "i") } }],
     });
+
+    for (const obj of returnedData) {
+      obj.password = null;
+    }
+
+    if (returnedData) return res.status(200).send(returnedData);
+    res.status(200).send({ code: "notFound" });
+  } catch (err) {
+    res.status(400).send("Error" + err);
+  }
+});
+
+router.post("/findById", async (req: Request, res: Response) => {
+  const { id: _id } = req.body;
+  if (isEmpty(_id)) return res.status(400).send({ code: "empty" });
+
+  try {
+    const returnedData = await userModel.findOne({ _id });
+    returnedData.password = null;
+
     if (returnedData) return res.status(200).send(returnedData);
     res.status(200).send({ code: "notFound" });
   } catch (err) {
